e7f1710ce1f1ffa7c9e536c91a6ca64229351833
Viraj Shah <veer15102003@gmail.com>
2023-08-24 17:46:41 +0530

    feat: added a number of utils, got final preds

--- scripts/create_preds_streamlined.py
+++ scripts/create_preds_streamlined.py
@@ -12,6 +12,8 @@ stock_ids = []
 weekly_preds = []
 monthly_preds = []
 dates = []
+weekly_conf = []
+monthly_conf =[ ]
 for count, filename in enumerate(os.listdir(Source_Path)):
     df = pd.read_csv(f"../preds/1000_companies_preds/{filename}")
     df.sort_values(by="date",inplace=True)
@@ -20,16 +22,22 @@ for count, filename in enumerate(os.listdir(Source_Path)):
     stock_id = filename[:dot_index]
     weekly_pred = df.iloc[-20]['Weekly Predictions Trend']
     monthly_pred = df.iloc[-20]['Monthly Preds Trend']
+    monthly_acc = df.iloc[-20]['Monthly Accuracy']
+    weekly_acc = df.iloc[-20]['Weekly Accuracy']
     stock_ids.append(stock_id)
     weekly_preds.append(weekly_pred)
     monthly_preds.append(monthly_pred)
     dates.append(date)
+    monthly_conf.append(round(monthly_acc,2))
+    weekly_conf.append(round(weekly_acc,2))
 
 preds = pd.DataFrame()
 preds['Date'] = dates
 preds['Stock ID'] = stock_ids
 preds['Weekly Predictions'] = weekly_preds
 preds['Monthly Predictions'] = monthly_preds
+preds['Weekly Confidence'] = weekly_conf
+preds['Monthly Confidence'] = monthly_conf
 preds.to_csv("../preds/all_preds.csv", index=False)
 
         
---
+++ scripts/get_stock_trend.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+main_df = pd.read_csv("../preds/all_preds.csv")
+stock_ids = main_df["Stock ID"].to_list()
+dates = main_df["Date"]
+weekly_trend = [None for _ in range(len(stock_ids))]
+monthly_trend = [None for _ in range(len(stock_ids))]
+for i in tqdm(range(len(stock_ids))):
+    df = pd.read_csv(f"../data/1000_stocks/{stock_ids[i]}.csv")
+    stock_id = stock_ids[i]
+    date = dates[i]
+    column = df.loc[df['date'] == date]
+    weekly_index_1 = len(df) - 20
+    weekly_column_1 = df.loc[df.index == weekly_index_1]
+    weekly_close_1 = weekly_column_1.close.to_list()[0]
+    weekly_index_2 = len(df) - 19
+    weekly_column_2 = df.loc[df.index == weekly_index_2]
+    weekly_close_2 = weekly_column_2.close.to_list()[0]
+    weekly_val = weekly_close_2 - weekly_close_1
+    #######################
+    monthly_index_1 = len(df) - 15
+    monthly_column_1 = df.loc[df.index == monthly_index_1]
+    monthly_close_1 = monthly_column_1.close.to_list()[0]
+    monthly_index_2 = len(df) - 14
+    monthly_column_2 = df.loc[df.index == monthly_index_2]
+    monthly_close_2 = monthly_column_2.close.to_list()[0]
+    monthly_val = monthly_close_2 - monthly_close_1
+    if (weekly_val > 0):
+        weekly_trend[i] = ("Up")
+    elif (weekly_val < 0):
+        weekly_trend[i] = ("Down")
+    else:
+        weekly_trend[i] = ("Flat")
+            
+    if (monthly_val > 0):
+        monthly_trend[i] = ("Up")
+    elif (monthly_val < 0):
+        monthly_trend[i] = ("Down")
+    else:
+        monthly_trend[i] = ("Flat")
+
+main_df['Actual Weekly Trend'] = weekly_trend
+main_df['Actual Monthly Trend'] = monthly_trend
+# print(len([i for i in monthly_trend if i is not None]))
+
+main_df.to_csv("../preds/all_preds_with_actual.csv",index=False)
